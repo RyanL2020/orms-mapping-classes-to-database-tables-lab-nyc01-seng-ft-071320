c1dd610b1bbc57828ddb1411d186761e8d3ce41a
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-23 23:52:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id
   attr_reader :id
   def inititalize(name, grade, id=nil)
     @name = name
@@ -18,6 +18,10 @@ class Student
          SQL
      DB[:conn].execute(sql)
    end
+
+   def drop_table
+     
+   end 
 end
 # Remember, you can access your database connection anywhere in this class
 #  with DB[:conn]
