fd80dee517fc6c3fe5bc0f0aabf6ae6c556d1b65
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-23 23:48:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ class Student
 
   attr_accessor :name, :grade
   attr_reader :id
-  def inititalize(name, grade, id)
+  def inititalize(name, grade, id=nil)
     @name = name
     @grade = grade
-    @id = nil
+    @id = id
   end
 
   def self.create_table
