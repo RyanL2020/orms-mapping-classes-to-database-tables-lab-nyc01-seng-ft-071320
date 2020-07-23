ce63821bbcb65ccf59b087a6166ff7ef3c14a565
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-23 23:41:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,22 @@ class Student
   attr_reader :id
 
   def inititalize(name, grade, id = nil)
+
     @name = name
     @grade = grade
-    @id = id
+    @id = nil
   end
+
+  def self.create_table
+     sql =  <<-SQL
+       CREATE TABLE IF NOT EXISTS songs (
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         album TEXT
+         )
+         SQL
+     DB[:conn].execute(sql)
+   end
 end
 # Remember, you can access your database connection anywhere in this class
 #  with DB[:conn]
